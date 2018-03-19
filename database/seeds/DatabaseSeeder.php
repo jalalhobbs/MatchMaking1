61cc5532cb5c84aa7d6bedb1bd7d881ad4b4ef49
Jalal Hobbs <jalalhobbs@gmail.com>
2018-03-19 22:20:28 +1030

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class DatabaseSeeder extends Seeder
+{
+
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+
+
+    }
+}

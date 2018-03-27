eadee509265ced2c1d18f1571dca5ffba15f225c
justin <s3569486@student.rmit.edu.au>
2018-03-27 11:54:12 +1100

    M-203 Added welcome message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in as USER!
+                    Welcome {{ Auth::user()->firstName }}!
                 </div>
             </div>
         </div>

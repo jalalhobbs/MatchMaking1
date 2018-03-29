a791c131ef6d3e72c51f7506831a62f30759ad56
Garret Bourke <garath.bourke@emc.com>
2018-03-29 17:41:59 +0100

    When user registers a welcome mail is sent

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+
+<body>
+<h2>Welcome to Matchmaking1 {{ $user['firstName']." ".$user['lastName'] }}</h2>
+
+</body>
+
+</html>

f88bc8013f5b4b301ca7c02b23fbd67c2814614e
Darren Li <s3611514@student.rmit.edu.au>
2018-03-30 17:17:58 +1000

    ready for testing!
    Login and Register pages
    +
    Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>

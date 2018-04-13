3ad6b157133bf346525069c48be34f105c7f2614
Darren Li <s3611514@student.rmit.edu.au>
2018-04-14 09:51:32 +1000

    M-298 / Moderate Change to Testing Phase
    ******* Please re-run the following
    php artisan migrate:fresh
    php artisan db:seed
    
    Changelog:
    1. Finished Preferences
    2. Updated seeding
    3. NOTE: Can set the preferences once only but more work can be done on this next splint.
    
    If you have any other problems, tell Daz.

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -9,6 +9,8 @@ use Validator;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Session;
+
 
 class PreferenceController extends Controller
 {
@@ -57,7 +59,36 @@ class PreferenceController extends Controller
      */
     public function show($id)
     {
-        //Functionality Disabled by routes page
+        if (auth()->user()->id == $id)
+        {
+            $preferences = DB::table('preferences')->orderBy('preferenceCategoryId', 'asc')->get();
+            $userPreferences = DB::table('user_preferences')->where('userId', auth()->user()->id)
+                ->get();
+            $preferenceTypes = DB::table('preference_types')->get();
+
+
+            if ($userPreferences === null)
+            {
+                return redirect(route('preferences.edit', [auth()->user()->id]));
+            }
+            else
+            {
+                return view('preference.view')
+                    ->with('userPreferences', $userPreferences)
+                    ->with('preferences', $preferences)
+                    ->with('preferenceTypes', $preferenceTypes);
+            }
+
+
+
+
+
+        }
+        else
+        {
+            // Go home..
+            return redirect(route('home'));
+        }
 
     }
 
@@ -75,6 +106,7 @@ class PreferenceController extends Controller
 
             //Retrieve Data from DB
             $preferences = DB::table('preferences')->orderBy('preferenceCategoryId', 'asc')->get();
+
 
 
 
@@ -115,12 +147,21 @@ class PreferenceController extends Controller
                 $preferenceName = $preference->preferenceName;
                 //Determine type of question
                 //Pull up an appropriate view given the answer to above.
-                echo $preferenceTypes->preferenceTypeName;
-                return view('preference.edit')
-                    ->with('preferenceCategories', $preferenceCategories)
-                    ->with('preferenceTypes', $preferenceTypes)
-                    ->with('userPreference', $userPreference)
-                    ->with('preferenceName', $preferenceName);
+
+                if ($userPreference->answer === null||$userPreference->answerWeight === null)
+                {
+                    //redirect home page.
+
+                    return view('preference.edit')
+                        ->with('preferenceCategories', $preferenceCategories)
+                        ->with('preferenceTypes', $preferenceTypes)
+                        ->with('userPreference', $userPreference)
+                        ->with('preferenceName', $preferenceName);
+                }
+                else
+                {
+                    //Don't do anything because the preference has been answered previously.
+                }
 
                 /*switch ($preferenceTypes->preferenceTypeName)
                 {
@@ -149,8 +190,18 @@ class PreferenceController extends Controller
 
             }
 
+            //$preferences->session()->flash('status', '');
+            //redirect home page.
+            Session::flash('status', 'Your Preferences have been updated.');
+            return redirect(route('home'));
 
 
+
+        }
+        else
+        {
+            // Go home..
+            return redirect(route('home'));
         }
 
 
@@ -165,9 +216,60 @@ class PreferenceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //Validate Response
 
+
+        //Validate Response
+        $request->validate([
+
+            'answer' => 'required|integer|between: 0, 5',
+            'answerWeight' => 'required|integer|between: 0, 5',
+            'preferenceId' => 'required|integer|min:1',
+            ],
+
+
+            [
+
+                'preferenceName.required' => 'This box cannot be empty',
+                'answer.required' => 'This box cannot be empty',
+                'answerWeight.required' => 'This box cannot be empty',
+                'preferenceId.required' => 'This box cannot be empty',
+                'answer.between' => 'Answer in this box must be between 0 and 5 inclusive.',
+                'answerWeight.between' => 'Answer in this box must be between 0 and 5 inclusive.',
+                'preferenceId.min' => 'Answer in this box must be 1 or greater.',
+                'answer.integer' => 'Answer in this box must be a whole number.',
+                'answerWeight.integer' => 'Answer in this box must be a whole number.',
+                'preferenceId.integer' => 'Answer in this box must be a whole number.',
+
+
+
+            ]
+        );
+
+
+        //Writes update  to the DB
         //Begin saving the data of the correct preference.
+        //https://stackoverflow.com/questions/17084723/how-to-pass-parameter-to-laravel-dbtransaction
+        DB::transaction(function() use ($request) {
+            DB::table('user_preferences')
+                ->where('userId', auth()->user()->id)
+                ->where('preferenceId', $request->preferenceId)
+                ->update(['answer' => $request->answer]);
+
+            DB::table('user_preferences')
+                ->where('userId', auth()->user()->id)
+                ->where('preferenceId', $request->preferenceId)
+                ->update(['answerWeight' => $request->answerWeight]);
+
+
+        });
+
+
+        //Determines where to go next
+        $request->session()->flash('status', 'Your Matchmaking Preference has been saved');
+        return redirect(route('preferences.edit', [auth()->user()->id]));
+        // You must complete all user_preference fields.
+
+
 
     }
 
--- resources/views/preference/view.blade.php
+++ resources/views/preference/view.blade.php
@@ -5,7 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">My Preferences about {{$preferenceCategories->preferenceCategoryName}}</div>
+                    <div class="card-header">My Preferences</div>
+
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,16 +15,17 @@
                             </div>
                         @endif
 
+                            @foreach($userPreferences as $userPreference)
                             <form method="POST" action="{{ route('preferences.update', [Auth::user()->id]) }}" id="preferenceForm">
                                 @csrf
                                 @method('PUT')
-
-
+                                @foreach($preferences as $preference)
+                                    @if($preference->id == $userPreference->preferenceId)
                                 <div class="form-group row">
                                     <label for="preferenceName" class="col-md-4 col-form-label text-md-right">{{ __('Question: ') }}</label>
 
                                     <div class="col-md-6">
-                                        <input id="preferenceName" type="text" class="form-control{{ $errors->has('preferenceName') ? ' is-invalid' : '' }}" name="preferenceName" value="{{ old('preferenceName', $preferenceName) }}" required disabled>
+                                        <input id="preferenceName" type="text" class="form-control{{ $errors->has('preferenceName') ? ' is-invalid' : '' }}" name="preferenceName" value="{{ old('preferenceName', $preference->preferenceName) }}" required disabled>
 
                                         @if ($errors->has('preferenceName'))
                                             <span class="invalid-feedback">
@@ -37,29 +39,32 @@
                                     <label for="answer" class="col-md-4 col-form-label text-md-right">{{ __('My Answer') }}</label>
 
                                     <div class="col-md-6">
-                                        <select id="answer" class="form-control{{ $errors->has('answer') ? ' is-invalid' : '' }}" name="answer" required>
+                                        <select id="answer" class="form-control{{ $errors->has('answer') ? ' is-invalid' : '' }}" name="answer" required disabled>
                                             @if($userPreference->answer === null)
                                                 <option selected value=""></option>
                                             @else
-                                                <option selected value="{{ $userPreference->answer }}"></option>
+                                                <option selected value="{{ $userPreference->answer }}">{{ $userPreference->answer }} out of 5</option>
                                             @endif
                                             @if(old('answer'))
                                                     <option selected value="{{ old('answer') }}"></option>
                                                 @endif
+                                                @foreach($preferenceTypes as $preferenceType)
+                                                    @if($userPreference->preferenceId == $preferenceType->id)
+                                                        @if($preferenceType->preferenceTypeName=="Normal")
+                                                            <option value="0">Not at all</option>
+                                                            <option value="1">Ever so slightly</option>
+                                                            <option value="2">Slightly</option>
+                                                            <option value="3">Mostly</option>
+                                                            <option value="4">Very much</option>
+                                                            <option value="5">Absolutely</option>
+                                                        @endif
+                                                        @if ($preferenceType->preferenceTypeName=="Boolean")
+                                                            <option value="5">Yes</option>
+                                                            <option value="0">No</option>
 
-                                                @if($preferenceTypes->preferenceTypeName=="Normal")
-                                                    <option value="0">Not at all</option>
-                                                    <option value="1">Ever so slightly</option>
-                                                    <option value="2">Slightly</option>
-                                                    <option value="3">Mostly</option>
-                                                    <option value="4">Very much</option>
-                                                    <option value="5">Absolutely</option>
-                                                @endif
-                                                @if ($preferenceTypes->preferenceTypeName=="Boolean")
-                                                    <option value="5">Yes</option>
-                                                    <option value="0">No</option>
-
-                                                @endif
+                                                        @endif
+                                                    @endif
+                                                @endforeach
 
 
 
@@ -78,11 +83,11 @@
                                         <label for="answerWeight" class="col-md-4 col-form-label text-md-right">{{ __('Importance to me') }}</label>
 
                                         <div class="col-md-6">
-                                            <select id="answerWeight" class="form-control{{ $errors->has('answerWeight') ? ' is-invalid' : '' }}" name="answerWeight" required>
+                                            <select id="answerWeight" class="form-control{{ $errors->has('answerWeight') ? ' is-invalid' : '' }}" name="answerWeight" required disabled>
                                                 @if($userPreference->answerWeight === null)
                                                     <option selected value=""></option>
                                                     @else
-                                                    <option selected value="{{ $userPreference->answerWeight }}"></option>
+                                                    <option selected value="{{ $userPreference->answerWeight }}">{{ $userPreference->answerWeight }} out of 5</option>
 
                                                 @endif
                                                 @if(old('answerWeight'))
@@ -90,19 +95,23 @@
 
 
                                                 @endif
-                                                @if($preferenceTypes->preferenceTypeName=="Normal")
-                                                    <option value="0"> Not at all important to me</option>
-                                                    <option value="1"> Ever so slightly important to me</option>
-                                                    <option value="2"> Slightly important to me</option>
-                                                    <option value="3"> Mostly important to me</option>
-                                                    <option value="4"> Very much important to me</option>
-                                                    <option value="5"> Absolutely important to me</option>
-                                                @endif
-                                                @if ($preferenceTypes->preferenceTypeName=="Boolean")
-                                                        <option value="5"> Absolutely important to me</option>
-                                                        <option value="0"> Not at all important to me</option>
+                                                @foreach($preferenceTypes as $preferenceType)
+                                                    @if($userPreference->preferenceId == $preferenceType->id)
+                                                        @if($preferenceType->preferenceTypeName=="Normal")
+                                                            <option value="0"> Not at all important to me</option>
+                                                            <option value="1"> Ever so slightly important to me</option>
+                                                            <option value="2"> Slightly important to me</option>
+                                                            <option value="3"> Mostly important to me</option>
+                                                            <option value="4"> Very much important to me</option>
+                                                            <option value="5"> Absolutely important to me</option>
+                                                        @endif
+                                                        @if ($preferenceType->preferenceTypeName=="Boolean")
+                                                                <option value="5"> Absolutely important to me</option>
+                                                                <option value="0"> Not at all important to me</option>
 
-                                                @endif
+                                                        @endif
+                                                    @endif
+                                                @endforeach
 
 
                                             </select>
@@ -126,6 +135,8 @@
                                         @endif
                                     </div>
                                 </div>
+                                    @endif
+                                    @endforeach
 
 
 
@@ -141,16 +152,11 @@
 
 
 
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4 text-md-right">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Confirm') }}
-                                        </button>
-                                    </div>
-                                </div>
+
 
 
                             </form>
+                                @endforeach
                     </div>
 
 

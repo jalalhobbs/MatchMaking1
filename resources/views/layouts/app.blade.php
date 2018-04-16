9ee6b14c0609f400c19524ead468d78ed343bc25
Jalal Hobbs <jalalhobbs@gmail.com>
2018-04-16 11:50:22 +0930

    M-202 rework - Welcome page, updated .env.example to contain facebook keys and webpack.mix.js to make sure of Laravel Mix and copy files from resources/assets/images to public/images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img class="logo" src="{{asset('/images/love.png')}}">
                     Match Maker One
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
